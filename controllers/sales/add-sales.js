9898e39259bc0b63e628b4bfdf1d20dbfa8b1915
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-01-10 18:33:55 +0100

    fix-p-barcode

--- controllers/sales/add-sales.js
+++ controllers/sales/add-sales.js
@@ -24,8 +24,12 @@ const addSales = async(req,res,next)=>{
        /****check if the purchased qty is not greater than the current qty */
        /***find product, deduct the qty from the current qty */
        const mproduct = await product.findProductByBarcode(mSales.product_barcode);
+       if (!mproduct) {
+        const err= new HttpError(400, `No product is assigned to the provided barcode`);
+        return next(err); 
+       }
 
-       if (mSales&&mSales.purchased_qty > mproduct.current_shipping_quauntity) {
+       if (mSales.purchased_qty > mproduct.current_shipping_quauntity) {
         const err= new HttpError(500, `The purchased quantity is greater than number of product in stock. You have ${mproduct.current_shipping_quauntity} left in stock`);
         return next(err);
        }
@@ -35,11 +39,7 @@ const addSales = async(req,res,next)=>{
                 current_shipping_quauntity: mproduct.current_shipping_quauntity - mSales.purchased_qty,
                 previous_shipping_quantity: mproduct.current_shipping_quauntity
             }  
-        }else{
-            const err= new HttpError(400, `No product is associated with the provided barcode`);
-            return next(err); 
-        }
-       const updateProduct = product.manageProductSales(mSales.product_barcode,data);
+            const updateProduct = product.manageProductSales(mSales.product_barcode,data);
        if (updateProduct) {
         addNewSales.save().then((s)=>{
             httpResponse({status_code:200, response_message:'Sales successfully added',data:s,res});
@@ -47,7 +47,12 @@ const addSales = async(req,res,next)=>{
             const err= new HttpError(500, e.message);
             return next(err);
            });
-          preSave(); //add branch to the sales
+          preSave();
+        }else{
+            const err= new HttpError(400, `No product is associated with the provided barcode`);
+            return next(err); 
+        }
+        //add branch to the sales
        }else{
         const err= new HttpError(500, 'Unable to add sales due to internal error, contact support');
         return next(err);
