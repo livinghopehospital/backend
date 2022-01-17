16a239597fc17b0da3c86c408f970775f5820cf6
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-01-17 20:32:47 +0100

    fixing-cors

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE,PATCH')
     res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-with,Content-Type,Accept,Authorization");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE,PATCH')
     next();
   });
 
