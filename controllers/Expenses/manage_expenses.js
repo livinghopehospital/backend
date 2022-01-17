49f4667f7814bbf0e74ff78965c8401f81c8c671
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-01-17 15:04:56 +0100

    add-expenses-update

--- controllers/Expenses/manage_expenses.js
+++ controllers/Expenses/manage_expenses.js
@@ -1,21 +1,44 @@
-
-
-
+const joi = require('joi');
+const {  Expenses } = require("../../model/Expenses/expenses")
+const { HttpError } = require("../../middlewares/errors/http-error");
+const joiError = require("../../middlewares/errors/joi-error");
+const { httpResponse } = require("../../middlewares/http/http-response");
+const Validation = joi.object({
+    date: joi.date(),
+    amount: joi.number(),
+    expenses_type: joi.string(),
+    additional_details: joi.string(),
+    branch_name: joi.string() 
+})
 
 const EditExpenses = async(req,res,next)=>{
     try {
-        
+        const {id} = req.params
+        const expensesUpdated =await Expenses.updateExpenses(id,Validation);
+        if (expensesUpdated) {
+            httpResponse({status_code:200, response_message:'This expenses as been updated successfully', data:expensesUpdated, res});
+        }else{
+            const e = new HttpError(400, 'Unable to update this expenses. Please contact support');
+            return next(e);
+        }
     } catch (error) {
-        
+        joiError(error,next)
     }
 }
 
 
 const deleteExpenses = async(req,res,next)=>{
     try {
-        
+        const {id} = req.params;
+        const expensesDeleted =await Expenses.updateExpenses(id);
+        if (expensesDeleted) {
+            httpResponse({status_code:200, response_message:'Expenses successfully deleted'})
+        }else{
+            const e = new HttpError(400, "Unable to delete this expenses. Contact support if persists");
+            return next(e);
+        }
     } catch (error) {
-        
+     joiError(error, next);   
     }
 }
 
