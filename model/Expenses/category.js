836d6fb4712d6a9d9363afac5a2a77e3d9ef4586
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-01-17 13:54:00 +0100

    fix-ep

--- model/Expenses/category.js
+++ model/Expenses/category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
    
-    category: {type: String, required:true}
+    expenses_category: {type: String, required:true}
 
 });
 
