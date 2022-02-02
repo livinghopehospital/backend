18bada2c5eeabcbb6d25b30828f76b8e4c69312f
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-02-02 21:44:36 +0100

    delete-sales-imprv

--- controllers/sales/delete-sales.js
+++ controllers/sales/delete-sales.js
@@ -1,5 +1,6 @@
 const { HttpError } = require("../../middlewares/errors/http-error");
 const { httpResponse } = require("../../middlewares/http/http-response");
+const { product } = require("../../model/products/products");
 const { Sales } = require("../../model/sales/sales")
 
 
@@ -13,9 +14,21 @@ const deleteSale = async function deleteSale(req,res,next){
             const e = new HttpError(400, "please supply id");
             return next(e);
         }
+        /****product should increase by the quantity in d sales deleted */
         const mSales = await Sales.deleteSales(id, branch_id);
         if (mSales) {
-            httpResponse({status_code:200, response_message:"sales successfully deleted", data:mSales, res});
+            const findProduct = await product.findOne({_id:mSales.product_id})
+            const data = {
+                current_product_quantity: findProduct.current_product_quantity + Number(mSales.quantity),
+                previous_product_quantity: findProduct.current_product_quantity
+            }
+            product.updateProduct(mSales.product_id,data).then(async(updated)=>{
+                httpResponse({status_code:200, response_message:"Sales successfully deleted", data:{}, res});
+                 return;
+             }).catch((e)=>{
+                 const err = new HttpError(500, e.message);
+                 return next(err);
+                 });
         }else{
             const e = new HttpError(500, "Unable to delete sales. Contact support if persists");
             return next(e);
