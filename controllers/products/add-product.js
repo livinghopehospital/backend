ac4fcd445094b121e664787ea621322214d20064
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-02-06 09:54:42 +0100

    fix-product-add

--- controllers/products/add-product.js
+++ controllers/products/add-product.js
@@ -5,36 +5,47 @@ const { product, productFieldValidation, productPreSaveHook } = require("../../m
 
 
 
-const addProducts = async(req,res,next)=>{
+const addProducts = async (req, res, next) => {
     try {
-        const {branch_id} = req.userData;
-        const mProduct =await productFieldValidation.validateAsync(req.body);
-        const productExists = await product.findProductByBarcode(mProduct.product_barcode,branch_id);
+        const { branch_id } = req.userData;
+        const { product_name,
+            product_price,
+            product_barcode,
+            product_brand,
+            supplier } = req.body
+        const mProduct = {
+            product_name,
+            product_barcode,
+            product_price,
+            supplier,
+            product_brand
+        }
+        const productExists = await product.findProductByBarcode(mProduct.product_barcode, branch_id);
         if (productExists) {
-        const err = new HttpError(400, 'A product already exists with the barcode');
-        return next(err);  
+            const err = new HttpError(400, 'A product already exists with the barcode');
+            return next(err);
         }
         const addNewProducts = product.createProduct(mProduct);
         const data = {
             branch_id,
         }
-        
-        addNewProducts.save().then(async(addedProduct)=>{
+
+        addNewProducts.save().then(async (addedProduct) => {
             // console.log(branch_id);
-            const p = await product.findOneAndUpdate({_id: addedProduct._id}, {branch:branch_id}, {upsert:true});
-            httpResponse({status_code:200, response_message:'Product added', data:p,res}); 
-          
-        }).catch((err)=>{
+            const p = await product.findOneAndUpdate({ _id: addedProduct._id }, { branch: branch_id }, { upsert: true });
+            httpResponse({ status_code: 200, response_message: 'Product added', data: p, res });
+
+        }).catch((err) => {
             const error = new HttpError(400, err.message);
-            return next(error);  
+            return next(error);
         });
-    
+
     } catch (error) {
         console.log(error);
-      joiError(error,next);  
+        joiError(error, next);
     }
 }
 
-module.exports={
+module.exports = {
     addProducts
 }
