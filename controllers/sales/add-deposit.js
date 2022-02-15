1d293daf38967e5a16f29a3e1c2f3c05133a433f
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-02-15 15:40:51 +0100

    deleting-unused-code

--- controllers/sales/add-deposit.js
+++ controllers/sales/add-deposit.js
@@ -76,36 +76,3 @@ module.exports={
     addDeposit
 }
 
-
-
-
-
-// mDeposit.items.forEach(async(item)=>{
-//     const mproduct = await findProduct(item.barcode, item.id);
-//     if (!mproduct) {
-//         const err= new HttpError(400, `No product is assigned to the provided barcode`);
-//         return next(err); 
-//        }
-//        if (item.quantity > mproduct.current_product_quantity) {
-//         const err= new HttpError(400, `The purchased quantity is greater than number of product in stock. You have ${mproduct.current_product_quantity} left in stock`);
-//         return next(err);
-//        }
-//         if (mproduct) {
-//             const data = {
-//                 current_product_quantity: mproduct.current_product_quantity -Number(item.quantity),
-//                 previous_product_quantity: mproduct.current_product_quantity
-//             }  
-//            product.manageProductSales(item.barcode,data,branch_id);
-    
-//        }else{
-//         const err= new HttpError(500, 'Unable to add Deposit due to internal error, contact support');
-//         return next(err);
-//        }
-//    });
-//    const ADDNEWDEPOSIT = Deposit.createDeposit(mDeposit);
-//       ADDNEWDEPOSIT.save().then((s)=>{
-//      httpResponse({status_code:201, response_message:'Deposit successfully created', res}) 
-//     }).catch((err)=>{
-//         const  ERROR =  new HttpError(500, err.message);
-//         return  next(ERROR);
-//     })
