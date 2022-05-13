6405b5746649439e9834184b8fbff53fb909dc28
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-05-13 10:18:35 +0100

    fix-deposit-response

--- controllers/Services-management/service-payment/fetch-deposit.js
+++ controllers/Services-management/service-payment/fetch-deposit.js
@@ -2,6 +2,7 @@
 const joiError = require("../../../middlewares/errors/joi-error");
 const { httpResponse } = require("../../../middlewares/http/http-response");
 const { servicePaymentDeposit } = require("../../../model/service-management/service-payment-deposit");
+const servicesRendered = require("../../../model/service/service");
 
 
 
@@ -10,7 +11,8 @@ const fetchDeposit = async function fetchDeposit(req,res,next){
         const {branch_id} = req.userData;
         const serviceDeposit  = await servicePaymentDeposit.fetchDeposit(branch_id);
         if (serviceDeposit) {
-      httpResponse({status_code:200, response_message:'Deposit records', data:{serviceDeposit}, res});
+     const serviceName = await servicesRendered.findOne({service_name:serviceDeposit.service_name})
+      httpResponse({status_code:200, response_message:'Deposit records', data:{...serviceDeposit, serviceName}, res});
         }
     } catch (error) {
         joiError(error,next)
