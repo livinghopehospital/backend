388695dfe256f81fcab8b6ed702524987914a3da
Mr Ahmad <cheapestorderconcept@gmail.com>
2022-05-29 17:26:28 +0100

    fix-deposit-tracks-bugs

--- controllers/Services-management/service-payment/fetch-deposit-track.js
+++ controllers/Services-management/service-payment/fetch-deposit-track.js
@@ -15,7 +15,7 @@ const fetchDepositTrack = async  function fetchDepositTrack(req,res,next){
         const {branch_id} = req.userData;
       const track = await   serviceDepositTrack.fetchDepositTrack(body.customer_name, branch_id);
       if (track&&track.length>0) {
-        httpResponse({status_code:200, response_message:'Deposit track record available', data:{track}, res});
+        httpResponse({status_code:200, response_message:'Deposit track record available', data:track, res});
       }else{
           return next(new HttpError(404, 'Customer currently have not made any deposits'));
       }
