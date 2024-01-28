319956eace093ff1e3b0006b516798cefbadde54
Mr Ahmad <cheapestorderconcept@gmail.com>
2024-01-28 11:14:55 +0100

    fix-bug

--- controllers/customers/view-customers.js
+++ controllers/customers/view-customers.js
@@ -26,7 +26,11 @@ const viewSingleCustomers = async function viewSingleCustomers(req, res, next) {
 
     try {
         const { branch_id } = req.userData;
-        const {reg_id} = req.body;
+        const {reg_id} = req.query;
+        if(!reg_id){
+            const e = new HttpError(400, 'reg_id is required');
+          return next(e);   
+        }
         const customers = await Customer.viewSingleCustomer(branch_id,reg_id);
         if (customers) {
             httpResponse({ status_code: 200, response_message: 'Customers available', data: { customers }, res });   
